e8a4ea949a4951871dbdc0661c9a810a5f368490
carolinaoliveirasilveira <carolinaoliveirasilveira@outlook.com>
2025-03-17 15:01:20 -0300

    structure directories and DTOs for microservices

---
+++ SalesHub/product-validation-service/src/main/java/br/com/microservices/orchestrated/productvalidationservice/core/dto/Event.java
@@ -0,0 +1,31 @@
+package br.com.microservices.orchestrated.orchestratorservice.core.dto;
+
+import br.com.microservices.orchestrated.orchestratorservice.core.enums.EEventSource;
+import br.com.microservices.orchestrated.orchestratorservice.core.enums.ESagaStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "event")
+public class Event {
+
+    @Id
+    private String id;
+    private String transactionId;
+    private String orderId;
+    private Order payload;
+    private EEventSource source;
+    private ESagaStatus status;
+    private List<History> eventHistory;
+    private LocalDateTime createdAt;
+}
